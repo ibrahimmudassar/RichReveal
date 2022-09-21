ae1916e4bd0ff353d85745ef493254c2de72ee3f
Anbar Saleem <45743448+anbarsaleem@users.noreply.github.com>
2022-09-21 17:31:43 -0400

    Fixed web scraping issue
    Now the chrome driver is used to headlessly load the site and grab the html

--- forbesScraper.py
+++ forbesScraper.py
@@ -1,10 +1,16 @@
 import requests;
 from bs4 import BeautifulSoup;
+from selenium import webdriver;
+from webdriver_manager.chrome import ChromeDriverManager
 
 #Retrieve Forbes page content
-URL = "https://www.forbes.com/real-time-billionaires/#7ff1165e3d78";
-page = requests.get(URL);
-soup = BeautifulSoup(page.content, "html.parser");
+URL = "https://www.forbes.com/real-time-billionaires/";
+#page = requests.get(URL);
+browser = webdriver.Chrome(ChromeDriverManager().install());
+browser.get(URL);
+html = browser.page_source;
+soup = BeautifulSoup(html, "lxml");
+print(soup.prettify());
 
 #Retrieve age and name info from website and organize into dictionary
 names = [];
@@ -15,9 +21,11 @@ for billionaire in billionaires:
     age = int(billionaire.find("td", class_="age").find("span", class_="ng-binding").get_text())
     
     if(age not in billiInfo.keys()):
-        billiInfo[age] = [[], 0];
+        billiInfo[age] = [[], 1];
     else:
         billiInfo[age][1] = billiInfo[age][1] + 1;
 
     name = billionaire.find("td", class_="name").find("a", class_="ng-binding").get_text();
     billiInfo[age][0].append(name);
+
+print(billiInfo);
